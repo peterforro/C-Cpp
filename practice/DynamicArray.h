cab2ae54f4261644d1afcbc5a88796210885a3ac
peterforro <peterforro@gmail.com>
2019-03-08 13:14:43 +0100

    DynamicArray self implementation

--- practice/DynamicArray.h
+++ practice/DynamicArray.h
@@ -3,116 +3,45 @@
 #include <iostream>
 
 
+template <typename T>
+class DynamicArray{
+    private:
+        T* pData;
+        unsigned size;
 
-template<typename T>
-class DynamicArray {
-private:
-    T* pData;
-    unsigned size;
-
-public:
-    DynamicArray(){
-        pData = nullptr;
-        size = 0;
-    }
-
-
-    DynamicArray(const T* initArray, unsigned size){
-        this->size = size;
-        pData = new T[size];
-        for(int i=0; i<size; ++i){
-            pData[i] = initArray[i];
+    public:
+        DynamicArray(){
+            this->size = 0;
+            pData = new T[size];
         }
-    }
 
-
-    ~DynamicArray(){
-        delete[] pData;
-    }
-
-
-    unsigned getSize(){
-        return size;
-    }
-
-
-    void add(T element){
-        T* tmp = new T[++size];
-        for(auto i=0; i<size-1; ++i){
-            tmp[i] = pData[i];
+        ~DynamicArray(){
+            delete[] pData;
         }
-        tmp[size-1] = element;
-        delete[] pData;
-        pData = tmp;
-    }
 
-
-    void stdOut()const{
-        for(auto i=0; i<size; ++i){
-            std::cout << pData[i] << std::endl;
+        void add(T element){
+            T *tmp = new T[++size];
+            for(int i=0; i<size-1; ++i){
+                tmp[i] = pData[i];
+            }
+            tmp[size-1] = element;
+            delete[] pData;
+//            pData = tmp;
         }
-    }
 
 
-    void clear(){
-        delete[] pData;
-        pData = nullptr;
-        size = 0;
-    }
-
-
-    T operator[](unsigned idx)const{
-        if(idx>size-1) throw "Error";
-        return pData[idx];
-    }
-
-
-    void insert(T element, unsigned idx){
-        if(idx>size-1) throw "Error";
-        T *tmp = new T[++size];
-        unsigned index = 0;
-        for(auto i=0; i<size; ++i){
-            if(i==idx){
-                tmp[index++]=element;
-                tmp[index++]=pData[i];
-            } else {
-                tmp[index++]=pData[i];
+        void stdOut(){
+            for(int i=0; i<size; ++i){
+                std::cout << pData[i] << std::endl;
             }
         }
-        delete[] pData;
-        pData = tmp;
-    }
+
+//        int *p = new int();
+//         delete p;
+//         delete p;
 
 
-    void operator+=(const DynamicArray<T>& rhs){
-        unsigned newSize = size + rhs.size;
-        T *tmp = new T[newSize];
-        unsigned index = 0;
-        for(auto i=0; i<size; ++i){
-            tmp[index++] = pData[i];
-        }
-        for(auto i=0; i<rhs.size; ++i){
-            tmp[index++] = rhs.pData[i];
-        }
-        delete[] pData;
-        pData = tmp;
-        size = newSize;
-    }
-
-
-    void remove(unsigned idx){
-        if(idx>size-1) throw "Error!";
-        auto tmp = new T[size-1];
-        unsigned index = 0;
-        for(auto i=0; i<size; ++i){
-            if(idx == i) continue;
-            tmp[index++] = pData[i];
-        }
-        delete[] pData;
-        pData = tmp;
-        --size;
-    }
 };
 
 
-#endif //PRACTICE_ARRAYLIST_H
+#endif
--- practice/main.cpp
+++ practice/main.cpp
@@ -3,15 +3,11 @@
 
 int main() {
 
-    int numbers1[] = {1,2,3,4,5,6,7,8,9,10};
-    int numbers2[] = {11,12,13,14,15,16};
-    auto list1 = new DynamicArray<int>(numbers1,10);
-    auto list2 = new DynamicArray<int>(numbers2,6);
+    DynamicArray<double> collection;
+    collection.add(10.69);
+    collection.add(11.345);
+    collection.stdOut();
 
-    (*list1).insert(69696969,3);
-    (*list1).remove(3);
-    (*list1) += (*list2);0
-    list1->stdOut();
-    std::cout << list1->getSize() << std::endl;
+
     return 0;
 }
